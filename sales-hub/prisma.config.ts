a33ef282cf314d3b4cda96060a5a4cbe39562ddd
Weslley Carlos de Morais <67909924+weslleycarlos@users.noreply.github.com>
2025-12-03 20:10:45 -0300

    feat: add Prisma configuration file for database connection

--- sales-hub/prisma.config.ts
+++ sales-hub/prisma.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
     datasource: {
-        db: {
-            url: process.env.DATABASE_URL,
-        },
+        url: process.env.DATABASE_URL,
     },
 });
